03b88846156b215e4f96b481b8a94fda93e8a376
Andres Alonso <lcatcherd14a@gmail.com>
2026-02-18 09:43:26 -0600

    lab3
    final el lab

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string nombreEstudiante;
+        string nombreCurso;
+        string mensaje;
+
+        Console.Write("Ingrese el nombre del estudiante: ");
+        nombreEstudiante = Console.ReadLine();
+
+        Console.Write("Ingrese el nombre del curso: ");
+        nombreCurso = Console.ReadLine();
+
+        Console.Write("Ingrese un mensaje personalizado: ");
+        mensaje = Console.ReadLine();
+
+
+        Console.WriteLine();
+        Console.WriteLine("Nombre del estudiante: " + nombreEstudiante);
+        Console.WriteLine("Nombre del curso: " + nombreCurso);
+        Console.WriteLine("Mensaje: " + mensaje);
+
+        Console.WriteLine();
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
